c280bc3b0662a9027909da910305a2a7299cebb9
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2023-03-28 18:47:37 +0200

    improved code logic

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import PySimpleGUI as sg
+import Gameai
+import Gamepvp
+import Game
 
-def init_window():
+
+def init_window(size):
     layout = []
-    for row in range(3):
+    for row in range(size[0]):
         row_list = []
-        for col in range(3):
+        for col in range(size[1]):
             row_list.append(sg.Button(key="{0},{1}".format(row, col), button_color="grey", size=(20,10), border_width=1))
         layout.append(row_list)
     new_window = sg.Window('Minimax', layout)
-    return new_window
+    return new_window, layout
 
 
 sg.theme('DarkBlack')
@@ -18,12 +22,14 @@ layout = [
     [sg.Button('Start')]
 ]
 window = sg.Window('Choise', layout=layout)
-
+size_of_board = (3,3)
 while True:
     event, values = window.read()
     if event == 'Start':
-        window = init_window()
-    if event == sg.WIN_CLOSED:
+        window, layout = init_window(size_of_board)
+    elif event == sg.WIN_CLOSED:
         break
+    else:
+        pass
 
 window.close()
